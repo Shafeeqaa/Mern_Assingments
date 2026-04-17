7515451169ac856bf0b6bf586827c7a41a075829
ShafeeqaBanu <shafeeqabanu19@gmail.com>
2026-04-17 09:26:24 +0530

    Add files via upload

---
+++ 20marchmvcapp/routes/productRoutes.js
@@ -0,0 +1,11 @@
+// routes/productRoutes.js
+const express = require("express");
+const router = express.Router();
+const { getAllProducts, getProductById, addProduct } = require("../controllers/productController");
+
+// Routes
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.post("/", addProduct);
+
+module.exports = router;
