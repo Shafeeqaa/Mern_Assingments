18230514f0b44fb7847bb392dd03f44abaacac8a
ShafeeqaBanu <shafeeqabanu19@gmail.com>
2026-04-17 09:27:26 +0530

    Add files via upload

---
+++ 20marchmvcapp/models/productModel.js
@@ -0,0 +1,8 @@
+// models/productModel.js
+let products = [
+  { id: 1, name: "Laptop", price: 50000, category: "Electronics" },
+  { id: 2, name: "Phone", price: 20000, category: "Electronics" },
+  { id: 3, name: "Chair", price: 3000, category: "Furniture" }
+];
+
+module.exports = products;
